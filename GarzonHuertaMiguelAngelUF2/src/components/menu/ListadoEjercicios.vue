714aa6099878426d31a3f80b4ed6b8f2d5c309e4
GarzonMiguelA <miguel.garzon.7e7@itb.cat>
2025-03-12 19:16:39 +0100

    commit

---
+++ GarzonHuertaMiguelAngelUF2/src/components/menu/ListadoEjercicios.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <h3>Examen UF2</h3>
+
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/aigua">Aigua</RouterLink>
+        <RouterLink to="/ListaCompra">ListaCompra</RouterLink>
+    </nav>
+</template>
+
+<style scoped>
+.menu-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 1rem;
+  background-color: #f8f9fa;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+nav a {
+  margin: 0 1rem;
+  text-decoration: none;
+  color: #007bff;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ GarzonHuertaMiguelAngelUF2/src/views/ListadoView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ListadoEjercicios from '@/components/menu/ListadoEjercicios.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+
+<template>
+   <div>
+        <ListadoEjercicios/>
+        <RouterView/>
+    </div>
+</template>
